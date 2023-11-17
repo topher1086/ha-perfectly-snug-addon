fcffe044e129ceb60387d2cb750094ea9f1b1d3f
Chris Christensen <christensenc@outlook.com>
2023-11-17 00:33:02 +0000

    Updating play and nap modes

--- perfectly-snug/snuggler.py
+++ perfectly-snug/snuggler.py
@@ -5,6 +5,7 @@ import sys
 import asyncio
 import json
 from datetime import datetime, timedelta
+from time import time
 from os import environ
 import traceback
 import pytz
@@ -31,17 +32,20 @@ logger.add(sys.stderr, level=settings['logging_level'] if in_addon else 'DEBUG')
 
 base_url = 'http://supervisor/core/api' if in_addon else 'http://localhost:8123/api'
 
+snuggler_url = f"ws://{settings['topper_ip_address']}/PSWS"
+
 logger.debug(f'Base URL: {base_url}')
 
 if in_addon:
     token = environ.get('SUPERVISOR_TOKEN')
 else:
-    token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiIxNGVhYzhiNjdmOTM0MDZmODYwNmFkOTcwOGJlNDgzOSIsImlhdCI6MTY5OTgyODUwNiwiZXhwIjoyMDE1MTg4NTA2fQ.q9jEO7QIpgSpUYd-uhqxtYkBolq8kJI1xzf2bJvl_fA'
+    token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJkYTMzYmFkY2UwMWE0OTk5ODJhOTlkMjljMmQzM2JhMiIsImlhdCI6MTcwMDE3MjUwOCwiZXhwIjoyMDE1NTMyNTA4fQ.FMQsbWDIOp3yXhSyXCP9IQhrkLW-UGxiskGrt9szjWU'
 
 request_side = '{"Comm":"Status","sideID":"?","Val":"Side"}'
 tx_id = 1
 
 tz = 'America/Chicago'
+
 
 def now():  # sourcery skip: aware-datetime-for-utc
     utc_now = pytz.utc.localize(datetime.utcnow())
@@ -57,6 +61,41 @@ def get_new_tx_id():
     return tx_id
 
 
+class TopperState():
+
+    def __init__(self) -> None:
+        pass
+
+class StatusHolder():
+
+    def __init__(self, switch_type) -> None:
+
+        self.switch_type = switch_type
+        self.check_timestamp = now()
+        self.last_mode_on = False
+
+        if switch_type == 'bedtime':
+            self.last_weekday_time = now()
+            self.last_weekend_time = now()
+
+        else:
+            logger.debug(f'{switch_type} created')
+
+            self.overnight = {'R': {}, 'L': {}}
+            self.overnight = {'R': {}, 'L': {}}
+
+            self.schedule = {'R': {}, 'L': {}}
+            self.schedule = {'R': {}, 'L': {}}
+
+            # app is -10 -> +10
+            # variables are 0 -> 20
+            # -2 in the app is an 8
+            self.run_level = 8
+
+bedtime_status = StatusHolder(switch_type='bedtime')
+nap_time_status = StatusHolder(switch_type='naptime')
+play_time_status = StatusHolder(switch_type='playtime')
+
 
 
 async def change_end_time(websocket, wake_up_time_dict, bedtime_mode_on, bedtime_mode_triggered, reset_start_times):
@@ -69,7 +108,7 @@ async def change_end_time(websocket, wake_up_time_dict, bedtime_mode_on, bedtime
 
     for side in ['R', 'L']:
 
-        logger.debug(f'Side {side} -- Checking settings')
+        logger.debug(f'{side} side -- Checking settings')
 
         # check the current running status
         for _ in range(5):
@@ -100,7 +139,7 @@ async def change_end_time(websocket, wake_up_time_dict, bedtime_mode_on, bedtime
 
         is_running = False
         if r_msg['Running'] == 1:
-            logger.debug(f'Side {side} -- is running')
+            logger.debug(f'{side} side -- is running')
             is_running = True
 
         # get the schedule settings now
@@ -145,19 +184,19 @@ async def change_end_time(websocket, wake_up_time_dict, bedtime_mode_on, bedtime
         # if bedtime mode has been turned on and the topper isn't running
         # then the start time was probably too late and it should be kicked off now
         if bedtime_mode_on and bedtime_mode_triggered and not is_running:
-            logger.warning(f'Side {side} -- Bedtime mode is on AND triggered recently AND the topper is NOT running')
+            logger.warning(f'{side} side -- Bedtime mode is on AND triggered recently AND the topper is NOT running')
             update_start_time = True
 
         if orig_r_msg != r_msg and bedtime_mode_on:
-            logger.info(f'Side {side} -- Change in settings with bedtime mode on, updating start times')
+            logger.info(f'{side} side -- Change in settings with bedtime mode on, updating start times')
             update_start_time = True
 
         if orig_r_msg != r_msg and is_running:
-            logger.info(f'Side {side} -- Change in settings with the topper running, updating start times')
+            logger.info(f'{side} side -- Change in settings with the topper running, updating start times')
             update_start_time = True
 
         if update_start_time:
-            logger.info(f'Side {side} -- Updating start times')
+            logger.info(f'{side} side -- Updating start times')
 
             start_time = now() + timedelta(minutes=2)
 
@@ -180,7 +219,7 @@ async def change_end_time(websocket, wake_up_time_dict, bedtime_mode_on, bedtime
             update_start_time = True
 
         if orig_r_msg != r_msg:
-            logger.info(f'Side {side} -- Sending message to update schedule')
+            logger.info(f'{side} side -- Sending message to update schedule')
             msg = json.dumps(r_msg, separators=(',', ':'))
             # logger.debug(f'Sending message: {msg}')
 
@@ -188,7 +227,7 @@ async def change_end_time(websocket, wake_up_time_dict, bedtime_mode_on, bedtime
             await asyncio.sleep(5)
 
         if update_start_time:
-            logger.info(f'Side {side} -- Update start time and it is running, stopping it')
+            logger.info(f'{side} side -- Update start time and it is running, stopping it')
 
             for _ in range(5):
 
@@ -217,7 +256,7 @@ async def change_end_time(websocket, wake_up_time_dict, bedtime_mode_on, bedtime
                     await asyncio.sleep(5)
 
             if r_msg['Running'] == 1:
-                logger.info(f'Side {side} running and schedule changed, stopping it')
+                logger.info(f'{side} side running and schedule changed, stopping it')
                 r_msg['Running'] = 0
 
                 msg = json.dumps(r_msg, separators=(',', ':'))
@@ -225,6 +264,152 @@ async def change_end_time(websocket, wake_up_time_dict, bedtime_mode_on, bedtime
 
                 await websocket.send(msg)
                 await asyncio.sleep(5)
+
+
+async def mode_changed_update_topper(websocket: websockets.WebSocketClientProtocol, status_obj: StatusHolder, mode_on: bool):
+
+    logger.info(f'{status_obj.switch_type} changed to {"ON" if mode_on else "OFF"}')
+
+
+    logger.debug(f'Sending: {request_side}')
+    await websocket.send(request_side)
+
+    _ = await recv_msg(websocket)
+    await asyncio.sleep(3)
+
+    if mode_on:
+        for side in ['R', 'L']:
+
+            logger.info(f'{side} side -- Getting settings to update and starting topper')
+
+            # check the current running status
+            overnight_success = False
+            for _ in range(10):
+
+                msg_s = {
+                    "Comm": "Status",
+                    "sideID": side,
+                    "Val": "Overnight",
+                    "TxId": get_new_tx_id()
+                }
+
+                msg = json.dumps(msg_s, separators=(',', ':'))
+                # logger.debug(f'Sending message: {msg}')
+
+                await websocket.send(msg)
+
+                # logger.debug('Msg sent')
+                # await asyncio.sleep(2)
+
+                overnight_msg = await recv_msg(websocket, 'overnight')
+
+                # logger.debug(f'Msg received: {overnight_msg}')
+
+                if overnight_msg['sideID'] == side:
+                    overnight_success = True
+                    break
+                else:
+                    await asyncio.sleep(5)
+
+            if overnight_msg['Running'] == 1:
+                logger.warning(f'{side} side is running, skipping')
+                break
+
+            if not overnight_success:
+                logger.warning(f'Could not get overnight status for {side} side, skipping')
+                break
+
+            # set the status in the object to revert back to
+            status_obj.overnight[side] = overnight_msg.copy()
+
+            # get the schedule settings now
+            schedule_success = False
+            for _ in range(10):
+
+                msg_s = {
+                    "Comm": "Status",
+                    "sideID": side,
+                    "Val": "Settings",
+                    "TxId": get_new_tx_id()
+                }
+
+                msg = json.dumps(msg_s, separators=(',', ':'))
+                # logger.debug(f'Sending message: {msg}')
+
+                await websocket.send(msg)
+
+                # logger.debug('Msg sent')
+                # await asyncio.sleep(2)
+
+                schedule_msg = await recv_msg(websocket, 'schedule')
+
+                # logger.debug(f'Msg received: {schedule_msg}')
+
+                if schedule_msg['sideID'] == side:
+                    schedule_success = True
+                    break
+                else:
+                    await asyncio.sleep(5)
+
+            if not schedule_success:
+                logger.warning(f'Could not get schedule status for {side} side, skipping')
+                break
+
+            # set the status in the object to revert back to
+            status_obj.schedule[side] = schedule_msg.copy()
+
+            # update the schedule now
+            logger.info(f'{side} side -- Sending message to update schedule time')
+
+            schedule_msg['T1'] = 1
+            schedule_msg['TxId'] = get_new_tx_id()
+
+            msg = json.dumps(schedule_msg, separators=(',', ':'))
+
+            await websocket.send(msg)
+            await asyncio.sleep(1)
+
+            logger.info(f'{side} side -- Sending message to update levels')
+
+            # set to -2 level for now ########################
+            overnight_msg['Running'] = 1
+            overnight_msg['L1'] = status_obj.run_level
+            overnight_msg['L2'] = status_obj.run_level
+            overnight_msg['L3'] = status_obj.run_level
+            overnight_msg['TxId'] = get_new_tx_id()
+
+            msg = json.dumps(overnight_msg, separators=(',', ':'))
+
+            await websocket.send(msg)
+            await asyncio.sleep(1)
+
+    else:
+
+        for side in ['R', 'L']:
+
+            logger.info(f'{side} side -- Resetting settings and stopping topper')
+
+            # update the schedule now
+            logger.info(f'{side} side -- Sending message to update schedule time')
+
+            schedule_msg = status_obj.schedule[side].copy()
+            schedule_msg['TxId'] = get_new_tx_id()
+
+            msg = json.dumps(schedule_msg, separators=(',', ':'))
+
+            await websocket.send(msg)
+            await asyncio.sleep(1)
+
+            logger.info(f'{side} side -- Sending message to update levels')
+
+            overnight_msg = status_obj.overnight[side].copy()
+            overnight_msg['Running'] = 0
+            overnight_msg['TxId'] = get_new_tx_id()
+
+            msg = json.dumps(overnight_msg, separators=(',', ':'))
+
+            await websocket.send(msg)
+            await asyncio.sleep(1)
 
 
 def check_msg(m, m_type=None):
@@ -314,9 +499,22 @@ async def get_wake_up_times():
         'weekend_j': weekend_j
     }
 
-async def get_bedtime_mode():
+# async def get_bedtime_mode():
 
-    url = f"{base_url}/states/{settings['bedtime_mode_helper']}"
+#     url = f"{base_url}/states/{settings['bedtime_mode_helper']}"
+
+#     headers = {
+#         'Authorization': f'Bearer {token}',
+#         'Content-Type': 'application/json',
+#     }
+
+#     r = get(url, headers=headers)
+
+#     return r.json()
+
+async def get_switch_status(entity_id):
+
+    url = f"{base_url}/states/{entity_id}"
 
     headers = {
         'Authorization': f'Bearer {token}',
@@ -327,27 +525,7 @@ async def get_bedtime_mode():
 
     return r.json()
 
-
-
 async def snuggler_update():
-
-    # last_run_file_name = 'last_run_status.json'
-
-    snuggler_url = f"ws://{settings['topper_ip_address']}/PSWS"
-
-    # logger.debug(f'Snuggler instruction: {instruction}')
-
-    # get the last run time
-    # try:
-    #     with open(last_run_file_name) as f:
-    #         last_run_status = json.load(f)
-    # except FileNotFoundError:
-    last_run_status = {
-        'timestamp': now(),
-        'last_weekday_time': now(),
-        'last_weekend_time': now(),
-        'last_bedtime_mode_on': False
-    }
 
     reset_start_time_sec_window = int((settings['update_interval_secs'] * 3) + (settings['update_interval_secs'] / 2))
 
@@ -355,35 +533,23 @@ async def snuggler_update():
 
         try:
 
-            logger.debug('Checking status...')
+            st = time()
+            logger.debug('Checking bedtime status...')
 
-            # last_run_time = parse_dt(last_run_status['timestamp'])
-            last_weekday_time = last_run_status['last_weekday_time']
-            last_weekend_time = last_run_status['last_weekend_time']
-            # last_bedtime_mode_on = last_run_status['last_bedtime_mode_on']
+            last_weekday_time = bedtime_status.last_weekday_time
+            last_weekend_time = bedtime_status.last_weekend_time
 
-            bedtime_mode = await get_bedtime_mode()
+            bedtime_mode = await get_switch_status(settings['bedtime_mode_helper'])
 
             bedtime_mode_triggered = (now() - parse_dt(bedtime_mode['last_changed'])).total_seconds() < settings['update_interval_secs'] + (settings['update_interval_secs'] / 2)
             bedtime_mode_on = bedtime_mode['state'] == 'on'
 
-            # bedtime_mode_last_changed = parse_dt(max(bedtime_mode['last_changed'], bedtime_mode['last_updated']))
-
             wake_up_time_dict = await get_wake_up_times()
-            # logger.debug(f'Wake up time dict: {wake_up_time_dict}')
 
-            # weekday_wake_up_time_last_changed = parse_dt(max(wake_up_time_dict['weekday_j']['last_changed'], wake_up_time_dict['weekday_j']['last_updated']))  # noqa: E501
-            # weekend_wake_up_time_last_changed = parse_dt(max(wake_up_time_dict['weekend_j']['last_changed'], wake_up_time_dict['weekend_j']['last_updated']))  # noqa: E501
-
-            # wake_up_last_changed = max(weekday_wake_up_time_last_changed, weekend_wake_up_time_last_changed)
-
-            # set the timestamp now
-            last_run_status |= {
-                'timestamp': now(),
-                'last_weekday_time': wake_up_time_dict['weekday'],
-                'last_weekend_time': wake_up_time_dict['weekend'],
-                'last_bedtime_mode_on': bedtime_mode_on,
-            }
+            bedtime_status.check_timestamp = now()
+            bedtime_status.last_weekday_time = wake_up_time_dict['weekday']
+            bedtime_status.last_weekend_time = wake_up_time_dict['weekend']
+            bedtime_status.last_mode_on = bedtime_mode_on
 
             # check if the wake up time has been updated
             need_to_update = False
@@ -417,20 +583,113 @@ async def snuggler_update():
             if need_to_update:
                 async with websockets.connect(snuggler_url) as ws:
 
-                    # await asyncio.gather(change_end_time(ws, parsed_time), receive(ws))
                     await asyncio.gather(change_end_time(ws, wake_up_time_dict, bedtime_mode_on, bedtime_mode_triggered, reset_start_times))
 
             else:
                 logger.debug('Wakeup times have not changed, not updating')
 
-            # with open(last_run_file_name, 'w') as f:
-            #     json.dump(last_run_status, f)
+        except Exception as e:
+            logger.critical(traceback.print_exc())
+            logger.critical(f'Exception: {type(e)}: {e}')
+
+        # check nap time now
+        try:
+
+            logger.debug('Checking nap time status...')
+
+            if bedtime_status.last_mode_on:
+                logger.info('Bedtime mode is on so not checking nap time')
+
+            elif play_time_status.last_mode_on:
+                logger.info('Play time mode is on so not checking nap time')
+
+            else:
+
+                nap_time_off_delay_secs = 100
+
+                status_obj = nap_time_status
+
+                nap_mode = await get_switch_status(settings['nap_time_mode_helper'])
+
+                nap_mode_delay_over = (now() - parse_dt(nap_mode['last_changed'])).total_seconds() > nap_time_off_delay_secs
+                nap_mode_on = nap_mode['state'] == 'on'
+
+                status_obj.check_timestamp = now()
+
+                if not status_obj.last_mode_on and nap_mode_on:
+                    logger.info('Nap mode has been turned on, updating topper settings and starting it')
+                    status_obj.last_mode_on = nap_mode_on
+
+                    async with websockets.connect(snuggler_url) as ws:
+                        await asyncio.gather(mode_changed_update_topper(ws, status_obj, status_obj.last_mode_on))
+
+                elif status_obj.last_mode_on and not nap_mode_on and nap_mode_delay_over:
+                    logger.info('Nap mode has been turned off and delay over, reverting topper settings and stopping it')
+                    status_obj.last_mode_on = nap_mode_on
+
+                    async with websockets.connect(snuggler_url) as ws:
+                        await asyncio.gather(mode_changed_update_topper(ws, status_obj, status_obj.last_mode_on))
+
+                elif status_obj.last_mode_on and not nap_mode_on:
+                    # don't update the object status since we didn't turn it off yet
+                    logger.info(f'Nap mode has been turned off, but delay off of {nap_time_off_delay_secs:,} seconds not over yet')
+
 
         except Exception as e:
             logger.critical(traceback.print_exc())
             logger.critical(f'Exception: {type(e)}: {e}')
 
+        # check play time now
+        try:
+
+            logger.debug('Checking play time status...')
+
+            if bedtime_status.last_mode_on:
+                logger.info('Bedtime mode is on so not checking play time')
+
+            elif nap_time_status.last_mode_on:
+                logger.info('Play time mode is on so not checking play time')
+
+            else:
+
+                play_time_off_delay_secs = 300
+
+                status_obj = play_time_status
+
+                play_mode = await get_switch_status(settings['play_time_mode_helper'])
+
+                play_mode_delay_over = (now() - parse_dt(play_mode['last_changed'])).total_seconds() > play_time_off_delay_secs
+                play_mode_on = play_mode['state'] == 'on'
+
+                status_obj.check_timestamp = now()
+
+                if not status_obj.last_mode_on and play_mode_on:
+                    logger.info('Play mode has been turned on, updating topper settings and starting it')
+                    status_obj.last_mode_on = play_mode_on
+
+                    async with websockets.connect(snuggler_url) as ws:
+                        await asyncio.gather(mode_changed_update_topper(ws, status_obj, status_obj.last_mode_on))
+
+                elif status_obj.last_mode_on and not play_mode_on and play_mode_delay_over:
+                    logger.info('Play mode has been turned off and delay over, reverting topper settings and stopping it')
+                    status_obj.last_mode_on = play_mode_on
+
+                    async with websockets.connect(snuggler_url) as ws:
+                        await asyncio.gather(mode_changed_update_topper(ws, status_obj, status_obj.last_mode_on))
+
+                elif status_obj.last_mode_on and not play_mode_on:
+                    # don't update the object status since we didn't turn it off yet
+                    logger.info(f'Play mode has been turned off, but delay off of {play_time_off_delay_secs:,} seconds not over yet')
+
+
+        except Exception as e:
+            logger.critical(traceback.print_exc())
+            logger.critical(f'Exception: {type(e)}: {e}')
+
+
         logger.info(f"Waiting {settings['update_interval_secs']:,} seconds to check again...")
+
+        logger.debug(f'Time taken to check: {(time() - st) * 1000:,.0f} ms')
         await asyncio.sleep(settings['update_interval_secs'])
 
 
